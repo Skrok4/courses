945ef29a841d3374a26a672232149c9a77a84d69
Skrok4 <73699809+Skrok4@users.noreply.github.com>
2022-01-24 20:45:12 +0200

    Create task4.js

---
+++ Homework/task4.js
@@ -0,0 +1,10 @@
+export function strToNumber(str) {
+let array = str.split(" ")
+if(array.length != 3) {
+    return "Error"
+}
+if(['+', '-', '/', '*'].indexOf(array[1]) === -1){
+    return "Operator input wrong"
+}
+return `a: ${array[0]}, b: ${array[2]}`
+}
