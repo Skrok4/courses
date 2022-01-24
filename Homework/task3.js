04afe255110bc944f679a22a0666c0d900e65727
Skrok4 <73699809+Skrok4@users.noreply.github.com>
2022-01-24 20:44:55 +0200

    Create task3.js

---
+++ Homework/task3.js
@@ -0,0 +1,15 @@
+export function createPhoneNumber(numbers) {
+    let firstpart = ""
+    let secondpart = ""
+    let thirdpart = ""
+    for (var i = 0; i < numbers.length; i++) {
+        if (i < 3) {
+            firstpart += numbers[i].toString()
+        } else if (i >= 3 && i < 6) {
+            secondpart += numbers[i].toString()
+        } else if (i >= 6) {
+            thirdpart += numbers[i].toString()
+        }
+    }
+    return `(${firstpart}) ${secondpart}-${thirdpart}`
+}   
