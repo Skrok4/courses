4c510eb1e991c63c29725c5af79b3aa5e026c343
Skrok4 <73699809+Skrok4@users.noreply.github.com>
2022-01-24 20:43:54 +0200

    Add code of 1 task

--- Homework/task1.js
+++ Homework/task1.js
@@ -1,1 +1,23 @@
-
+export function basicOp(operation, a, b){
+    if (isNaN(a) || isNaN(b)) {
+        return 0
+    }
+    let numA = parseInt(a)
+    let numB = parseInt(b)
+    switch (operation) {
+        case "+":
+            return numA + numB
+    
+        case "-":
+            return numA - numB
+    
+        case "*":
+            return numA * numB
+    
+        case "/":
+            return numA / numB
+    
+        default:
+            return 0
+    }
+} 
