88d07f81376c8f755f66af33915e9d9a769763bd
Skrok4 <73699809+Skrok4@users.noreply.github.com>
2022-01-25 18:47:53 +0200

    Update name of variables

--- Homework/task2.js
+++ Homework/task2.js
@@ -1,7 +1,7 @@
 export function stringMerge(str1, str2, symbol)
 {
-    let a = str1.split(symbol)[0];
+    let before_symbol = str1.split(symbol)[0];
     let pos = str2.indexOf(symbol)
-    let b = str2.substr(pos,)
-    return a.concat(b);
+    let after_symbol = str2.substr(pos,)
+    return before_symbol.concat(after_symbol);
 }
