6dcca554cf8553edf42b98c61bd8552ee84c1c89
Shevchenko Alex <73699809+Skrok4@users.noreply.github.com>
2022-01-27 17:12:13 +0200

    Eslint fix

--- Homework/task4.js
+++ Homework/task4.js
@@ -1,10 +1,13 @@
 export function strToNumber(str) {
-let array = str.split(" ")
-if(array.length != 3) {
-    return "Error"
-}
-if(['+', '-', '/', '*'].indexOf(array[1]) === -1){
-    return "Operator input wrong"
-}
-return `a: ${array[0]}, b: ${array[2]}`
+  const array = str.split(' ');
+
+  if(array.length != 3) {
+    return 'Error';
+  }
+
+  if(['+', '-', '/', '*'].indexOf(array[1]) === -1){
+    return 'Operator input wrong';
+  }
+
+  return `a: ${array[0]}, b: ${array[2]}`;
 }
