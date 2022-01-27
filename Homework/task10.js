4841b3d0c733c7fe123f02d14649267404683d97
Shevchenko Alex <73699809+Skrok4@users.noreply.github.com>
2022-01-27 17:16:14 +0200

    Eslint fix

--- Homework/task10.js
+++ Homework/task10.js
@@ -1,12 +1,15 @@
 export function alphabetPosition(string){
-    let lettersPosition = '';
-    for (let index = 0; index < string.length; index++) {
-      if (string.charCodeAt(index) > 64 && string.charCodeAt(index) < 91) {
-        lettersPosition += `${(string.charCodeAt(index)) - 64} `;
-      }
-      if (string.charCodeAt(index) > 96 && string.charCodeAt(index) < 123) {
-        lettersPosition += `${(string.charCodeAt(index)) - 96} `;
-      }
+  let lettersPosition = '';
+
+  for (let index = 0; index < string.length; index++) {
+    if (string.charCodeAt(index) > 64 && string.charCodeAt(index) < 91) {
+      lettersPosition += `${(string.charCodeAt(index)) - 64} `;
     }
-    return lettersPosition.slice(0, -1);
+
+    if (string.charCodeAt(index) > 96 && string.charCodeAt(index) < 123) {
+      lettersPosition += `${(string.charCodeAt(index)) - 96} `;
+    }
   }
+
+  return lettersPosition.slice(0, -1);
+}
