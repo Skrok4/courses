96c4ba2ad81052fd4e622327eaeee33466244219
Shevchenko Alex <73699809+Skrok4@users.noreply.github.com>
2022-01-27 18:57:28 +0200

    delete ternar operator

--- Homework/task6.js
+++ Homework/task6.js
@@ -2,5 +2,5 @@ export function isPalindrome(str){
   const string = str.replace(/\W/g, '').toLowerCase();
   const reverseString = string.split('').reverse().join('');
 
-  return string === reverseString ? true : false;
+  return string === reverseString;
 }
