9d5d70e3b8d07ffffd0f2522434ec28424b0b508
Shevchenko Alex <73699809+Skrok4@users.noreply.github.com>
2022-01-27 17:13:06 +0200

    Update task5.js

--- Homework/task5.js
+++ Homework/task5.js
@@ -1,19 +1,23 @@
 export function getTime(str){ 
-    const listIdx = [];
-    let lastIndex = -1
-    while ((lastIndex = str.indexOf(':', lastIndex + 1)) !== -1) {
-      listIdx.push(lastIndex)
-    }
-    for (let index = 0; index < listIdx.length; index++){
-    const hours = str.substr(listIdx[index]-2, 2)
-    const minutes = str.substr(listIdx[index]+1, 2)
+  const listIdx = [];
+  let lastIndex = -1;
+
+  while ((lastIndex = str.indexOf(':', lastIndex + 1)) !== -1) {
+    listIdx.push(lastIndex);
+  }
+
+  for (let index = 0; index < listIdx.length; index++){
+    const hours = str.substr(listIdx[index]-2, 2);
+    const minutes = str.substr(listIdx[index]+1, 2);
+
     if (parseInt(hours) < 0 || parseInt(hours) >= 24) {
-        continue;
+      continue;
+    } else if (parseInt(minutes) < 0 || parseInt(minutes) >= 60) {
+      continue;
     }
-    else if (parseInt(minutes) < 0 || parseInt(minutes) >= 60) {
-        continue;
-    }
+
     return `${hours}:${minutes}`;
-} 
-return ""
+  }
+ 
+  return '';
 }
