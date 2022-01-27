c6a1f2234400f253ce33108bd06ffe549a7c947c
Shevchenko Alex <73699809+Skrok4@users.noreply.github.com>
2022-01-27 15:29:13 +0200

    Create task7.js

---
+++ Homework/task7.js
@@ -0,0 +1,11 @@
+export function swapCase(str) {
+    let newString = "";
+      for (let index = 0; index < str.length; index++) {
+        if (str[index] === str[index].toLowerCase()) {
+            newString += str[index].toUpperCase();
+        } else {
+            newString += str[index].toLowerCase();
+        }
+      }
+      return newString;
+}
