4ae1fae4c942bcd497e6c79f1f3658708850d2a8
Shevchenko Alex <73699809+Skrok4@users.noreply.github.com>
2022-01-27 15:30:14 +0200

    Create task9.js

---
+++ Homework/task9.js
@@ -0,0 +1,10 @@
+export function vowelCount(string) {
+    let counter = 0;
+  
+    for (const letter of string) {
+      if (letter.match(/a|e|i|o|u/)) {
+        counter++;
+      }
+    }
+    return counter;
+  }
