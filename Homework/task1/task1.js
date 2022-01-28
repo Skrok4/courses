940b74758dfb1f634317b826c072242a393cf6f0
Shevchenko Alex <73699809+Skrok4@users.noreply.github.com>
2022-01-28 19:42:06 +0200

    Create task1.js

---
+++ Homework/task1/task1.js
@@ -0,0 +1,24 @@
+export function basicOp(operation, a, b){
+  if (isNaN(a) || isNaN(b)) {
+    return 0;
+  }
+
+  const numA = parseInt(a);
+  const numB = parseInt(b);
+
+  switch (operation) {
+    case '+':
+      return numA + numB;
+    case '-':
+      return numA - numB;
+    
+    case '*':
+      return numA * numB;
+    
+    case '/':
+      return numA / numB;
+    
+    default:
+      return 0;
+  }
+}
