6d4bc0f5afbb6944d41678e82def8f3857ace28f
Shevchenko Alex <73699809+Skrok4@users.noreply.github.com>
2022-01-28 19:46:17 +0200

    Create task4.js

---
+++ Homework/task4/task4.js
@@ -0,0 +1,13 @@
+export function strToNumber(str) {
+  const array = str.split(' ');
+
+  if(array.length !== 3) {
+    return 'Error';
+  }
+
+  if(['+', '-', '/', '*'].indexOf(array[1]) === -1){
+    return 'Operator input wrong';
+  }
+
+  return `a: ${array[0]}, b: ${array[2]}`;
+}
