f568de8ce5ed6ceb4fd35ab415445267dc5ca5da
Shevchenko Alex <73699809+Skrok4@users.noreply.github.com>
2022-01-28 20:21:13 +0200

    Delete task5(reg).js

--- Homework/task5(reg).js
+++
@@ -1,6 +0,0 @@
-export function getTimeReg(timeStr){ 
-  const regPattern = /((\b[0-1][0-9]|2[0-3])+:[0-5][0-9]\b)(,|$)/g;
-  const regex = timeStr.match(regPattern);
-  
-  return regex !== null ? regex[0].replace(/,/, '') : '';
-}
