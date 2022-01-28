4635149e6dcbf18e55e13ecc511f3f8de3e95bec
Shevchenko Alex <73699809+Skrok4@users.noreply.github.com>
2022-01-28 20:21:42 +0200

    Delete task8.js

--- Homework/task8.js
+++
@@ -1,6 +0,0 @@
-export function countOccurrences(string, symbol) {
-  const regex = new RegExp(`\\${symbol}\\B|(${symbol})`, 'gi');
-  const counter = string.match(regex);
-
-  return counter.length;
-}
