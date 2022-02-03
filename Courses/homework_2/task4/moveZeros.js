2a0ee8d5ef6499b5b4fccca014fd5991f1284ecd
Shevchenko Alex <73699809+Skrok4@users.noreply.github.com>
2022-02-03 16:44:31 +0200

    Update moveZeros.js

--- Courses/homework_2/task4/moveZeros.js
+++ Courses/homework_2/task4/moveZeros.js
@@ -1,16 +1,16 @@
 export const moveZeros = (importArray) =>{
-  const indexes = [];
+  const arrayOfMatchesWith0 = [];
   const zero = 0;
 
   for(let index = 0; index < importArray.length; index++){
     if (importArray[index] === zero){
-      indexes.push(index);
+      arrayOfMatchesWith0.push(index);
     }
   }
-  indexes.reverse();
+  arrayOfMatchesWith0.reverse();
 
-  for(let index = 0; index < indexes.length; index++){
-    importArray.splice(indexes[index], 1);
+  for(let index = 0; index < arrayOfMatchesWith0.length; index++){
+    importArray.splice(arrayOfMatchesWith0[index], 1);
     importArray.push(zero);
   }
 
