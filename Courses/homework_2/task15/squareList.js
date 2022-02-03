9ff39ed4164c61c3271f8632d2f42275e8e792e7
Shevchenko Alex <73699809+Skrok4@users.noreply.github.com>
2022-02-03 19:59:05 +0200

    delete unnecessary array

--- Courses/homework_2/task15/squareList.js
+++ Courses/homework_2/task15/squareList.js
@@ -1,9 +1,5 @@
 export const squareList = (array) =>{
-    const result = [];
-  
-    result.push(array.map((item) => 
-      ((item > 0 && item % 1 == 0) ? item * item : null)).filter(Number));
     
-  
-    return result.flat();
-  };
+  return array.map((item) => 
+  ((item > 0 && item % 1 == 0) ? item * item : null)).filter(Number);
+};
