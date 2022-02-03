93c0950773123aa190ca6bd833cba2960d7adbbd
Shevchenko Alex <73699809+Skrok4@users.noreply.github.com>
2022-02-03 11:38:49 +0200

    Update calculateRentalCost.js

--- Courses/homework_2/task1/calculateRentalCost.js
+++ Courses/homework_2/task1/calculateRentalCost.js
@@ -1,5 +1,7 @@
 export const calculateRentalCost = (quantityOfDays)=>{
   const priceForDay = 40;
+  const discountMore2Days = 20;
+  const discountMore6Days = 50;
 
   if (quantityOfDays < 0){
     return 'Sorry! Quantity of days can\'t be less than 1';
@@ -10,9 +12,9 @@ export const calculateRentalCost = (quantityOfDays)=>{
     return priceForDay * quantityOfDays;
   } else if (quantityOfDays >= 3 && quantityOfDays < 7) {
 
-    return priceForDay * quantityOfDays - 20;
+    return priceForDay * quantityOfDays - discountMore2Days;
   } else if (quantityOfDays >= 7 ) {
 
-    return priceForDay * quantityOfDays - 50;
+    return priceForDay * quantityOfDays - discountMore6Days;
   } 
 };
