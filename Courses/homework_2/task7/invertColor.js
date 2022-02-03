ae8dae94abfdd62ff03e9f9393a7ec5bf4b70d12
Shevchenko Alex <73699809+Skrok4@users.noreply.github.com>
2022-02-03 11:53:16 +0200

    Update invertColor.js

--- Courses/homework_2/task7/invertColor.js
+++ Courses/homework_2/task7/invertColor.js
@@ -1,18 +1,18 @@
 export const invertColor = (colorInHexFormat) =>{
 
-    const hexSymbols = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
-    
-    let invertHex = '#';
+  const hexSymbols = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+  const hexSymbolsReverse = [...hexSymbols].reverse();
+  let invertHex = '#';
   
-    colorInHexFormat = colorInHexFormat.toLowerCase();
-  
-    for (let i = 0; i < colorInHexFormat.length; i++) {
-      const index = hexSymbols.indexOf(colorInHexFormat[i]);
-  
-      if (index >= 0) {
-        invertHex += hexSymbols.reverse()[index];
-      }
+  colorInHexFormat = colorInHexFormat.toLowerCase();
+
+  for (let i = 0; i < colorInHexFormat.length; i++) {
+    const index = hexSymbols.indexOf(colorInHexFormat[i]);
+
+    if (index >= 0) {
+      invertHex += hexSymbolsReverse[index];
     }
-  
-    return invertHex;
-  };
+  }
+
+  return invertHex;
+};
