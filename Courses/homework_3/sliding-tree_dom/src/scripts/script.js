7a9c314425bdaa2422965e5612ac0dcd3abb3924
Shevchenko Alex <73699809+Skrok4@users.noreply.github.com>
2022-02-15 00:12:01 +0200

    add hover effect

--- Courses/homework_3/sliding-tree_dom/src/scripts/script.js
+++ Courses/homework_3/sliding-tree_dom/src/scripts/script.js
@@ -1,5 +1,15 @@
 const tree = document.querySelector('.tree');
 
+const elements = document.querySelectorAll('li');
+const classToAdd = 'active';
+elements.forEach(element => element.addEventListener('mouseenter', ()=>{
+  element.classList.add(classToAdd);
+}));
+
+elements.forEach(element => element.addEventListener('mouseleave', ()=>{
+  element.classList.remove(classToAdd);
+}));
+
 tree.addEventListener('click', (event) => {
   if(event.target.tagName != 'LI') {return;}
   if(event.target.children.length > 0) {event.target.children[0].hidden = !event.target.children[0].hidden;}
