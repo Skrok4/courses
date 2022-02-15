e50a12e4ae196e163a3c4c3db4224c6516dcd013
Shevchenko Alex <73699809+Skrok4@users.noreply.github.com>
2022-02-15 16:50:16 +0200

    fix code, delete unnecessary elements

--- Courses/homework_4/2/index.js
+++ Courses/homework_4/2/index.js
@@ -1,50 +1,54 @@
-/* eslint-disable no-unused-vars */
 const usersURL = 'https://jsonplaceholder.typicode.com/users';
 const toDoListURL = 'https://jsonplaceholder.typicode.com/todos';
 
 const users = () =>{
   return fetch(usersURL).then(response => {
     return response.json();
-  })
+  });
 };
 
 const toDo = () =>{
   return fetch(toDoListURL).then(response => {
     return response.json();
-  })
+  });
 };
 
 const showComplete = () =>{
-let completedIdList = [];
-let completedList = [];
+  const completedIdList = [];
+  const completedList = [];
+
   toDo()
-.then(
-  data =>{
-    for (const [key, value] of Object.entries(data)){
-    if (value.completed === true){
-      completedIdList.push(value.id)
-    }
-  } return completedIdList
-})
-.catch((error) => {
-  console.log(error)
-});
+    .then(
+      data =>{
+        for (const [, value] of Object.entries(data)){
+          if (value.completed === true){
+            completedIdList.push(value.id);
+          }
+        }
+
+        return completedIdList;
+      })
+    .catch((error) => {
+      return error;
+    });
 
   users()
-.then(data =>{
-  for (const [key, value] of Object.entries(data)){
-    completedIdList.forEach(element => {
-      if(value.id === element){ 
-        let id = parseInt(element)
-        completedList.push(data[id-1])
+    .then(data =>{
+      for (const [, value] of Object.entries(data)){
+        completedIdList.forEach(element => {
+          if(value.id === element){ 
+            const id = parseInt(element);
+
+            completedList.push(data[id - 1]);
+          }
+        });
       }
-  })}
-  console.log(completedList)
-  return completedList;
-})
-.catch((error) => {
-  console.log(error)
-});
-}
+
+      return completedList;
+    })
+    .catch((error) => {
+      return error;
+    });
+};
 
 showComplete();
