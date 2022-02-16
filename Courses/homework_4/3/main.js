c3bdd3fc3a892bc7f379cec63c1afa49ac0563fb
Shevchenko Alex <73699809+Skrok4@users.noreply.github.com>
2022-02-17 01:28:19 +0200

    create third task folder

---
+++ Courses/homework_4/3/main.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+const usersURL = 'https://jsonplaceholder.typicode.com/users';
+const postsURL = 'https://jsonplaceholder.typicode.com/posts';
+const commentsURL = 'https://jsonplaceholder.typicode.com/comments';
+
+
+const getData = async(url) => {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch(error) {
+    return error;
+  }
+};
+
+const usersWithPostAndComments = async() => {
+  const users = await getData(usersURL);
+  const posts = await getData(postsURL);
+  const comments = await getData(commentsURL);
+
+  const userList = users.map(user => {
+    const userPosts = posts.filter(post => user.id === post.userId);
+
+    const userPostAndComments = userPosts.map(post => {
+      const commentsForPost = comments.filter((comment) => comment.postId === post.id);
+      return { ...post, comments: commentsForPost };
+    })
+
+    user.posts = userPostAndComments;
+
+    return user;
+  });
+
+  return userList;
+};
+
+usersWithPostAndComments()
