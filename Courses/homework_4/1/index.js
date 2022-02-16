34e9cb1d3493fefe78a8e5336c5ff5fea25fa7d0
Shevchenko Alex <73699809+Skrok4@users.noreply.github.com>
2022-02-17 01:21:58 +0200

    fix first task, async/await

--- Courses/homework_4/1/index.js
+++ Courses/homework_4/1/index.js
@@ -1,53 +1,26 @@
 const storeURL = 'https://fakestoreapi.com/products';
 
-const store = () =>{
-  return fetch( storeURL).then(response => {
-    return response.json();
+export const getData = async(url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    return error;
+  }
+};  
+
+const sortData = async() =>{
+  const unsortedList = await getData(storeURL);
+
+  const sortedList = unsortedList.sort((less, more) => more.price - less.price);
+
+  sortedList.sort((x, y) => {
+    return x.category.localeCompare(y.category);
   });
+
+  return sortedList;
 };
 
-const sortReceived = () =>{
-  const priceList = [];
-  let sortedList = [];
-  const categoryList  = [];
-  let sortedByAlphabet = [];
-
-  store()
-    .then(
-      data =>{
-        for (const [, value] of Object.entries(data)){
-          priceList.push(value.price);
-        }
-        priceList.sort((less, more) => more - less);
-    
-        priceList.forEach(element => {
-          for(let i = 0; i < data.length; i++){
-            if(data[i].price === element && data[i] != sortedList){
-              sortedList.push(data[i]);
-            }
-          }
-          [...sortedList] = new Set(sortedList);
-        });
-        
-        for (const [, value] of Object.entries(data)){
-          categoryList.push(value.category);
-        }
-        categoryList.sort();
-        
-        categoryList.forEach(element => {
-          for(let i = 0; i < sortedList.length; i++){
-            if(sortedList[i].category === element && sortedList[i] != sortedByAlphabet){
-              sortedByAlphabet.push(sortedList[i]);
-            }
-          }
-          [...sortedByAlphabet ] = new Set(sortedByAlphabet );
-        });
-
-        return sortedByAlphabet;
-      })
-    .catch((error) => {
-      return error;
-    });
-};
-
-sortReceived();
+sortData();
